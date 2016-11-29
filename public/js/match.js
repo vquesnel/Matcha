6675320bb998a92e400bbad2606de098901fa9a8
Victor QUESNEL <vquesnel@student.42.fr>
2016-11-29 17:05:12 +0100

    begin of sorting match results

--- public/js/match.js
+++ public/js/match.js
@@ -17,55 +17,75 @@ function errorHandler(error) {
     alert('Une erreur est survenue durant la géolocalisation. Veuillez réessayer plus tard ou contacter le support.');
 }*/
 $(document).ready(function () {
-    var socket = io.connect('https://localhost:4433');
-    var currgeocoder;
-    var iploc;
-    var currentloc = [];
-    socket.on("iplocation", function (data) {
-            iploc = (data.location);
-            initializeCurrent(iploc[0], iploc[1]);
-        })
-        //Set geo location lat and long
-    navigator.geolocation.getCurrentPosition(function (position, html5Error) {
-        geo_loc = processGeolocationResult(position);
-        currLatLong = geo_loc.split(",");
-        if (currLatLong) {
-            initializeCurrent(currLatLong[0], currLatLong[1]);
-        } else {}
-    });
-    //Get geo location result
-    function processGeolocationResult(position) {
-        html5Lat = position.coords.latitude; //Get latitude
-        html5Lon = position.coords.longitude; //Get longitude
-        html5TimeStamp = position.timestamp; //Get timestamp
-        html5Accuracy = position.coords.accuracy; //Get accuracy in meters
-        return (html5Lat).toFixed(8) + ", " + (html5Lon).toFixed(8);
-    }
-    //Check value is present or not & call google api function
-    function initializeCurrent(latcurr, longcurr) {
-        currgeocoder = new google.maps.Geocoder();
-        if (latcurr != '' && longcurr != '') {
-            var myLatlng = new google.maps.LatLng(latcurr, longcurr);
-            return getCurrentAddress(myLatlng);
-        }
-    }
-    //Get current address
-    function getCurrentAddress(location) {
-        currgeocoder.geocode({
-            'location': location
-        }, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                currentloc.push(results[0].formatted_address);
-                socket.emit("location", {
-                    location: currentloc
-                });
-            } else {
-                alert('Geocode was not successful for the following reason: ' + status);
-            }
-        });
-    }
+	var socket = io.connect('https://localhost:4433');
+	var currgeocoder;
+	var iploc;
+	var currentloc = [];
+	socket.on("iplocation", function (data) {
+			iploc = (data.location);
+			initializeCurrent(iploc[0], iploc[1]);
+		})
+		//Set geo location lat and long
+	navigator.geolocation.getCurrentPosition(function (position, html5Error) {
+		geo_loc = processGeolocationResult(position);
+		currLatLong = geo_loc.split(",");
+		if (currLatLong) {
+			initializeCurrent(currLatLong[0], currLatLong[1]);
+		}
+		else {}
+	});
+	//Get geo location result
+	function processGeolocationResult(position) {
+		html5Lat = position.coords.latitude; //Get latitude
+		html5Lon = position.coords.longitude; //Get longitude
+		html5TimeStamp = position.timestamp; //Get timestamp
+		html5Accuracy = position.coords.accuracy; //Get accuracy in meters
+		return (html5Lat).toFixed(8) + ", " + (html5Lon).toFixed(8);
+	}
+	//Check value is present or not & call google api function
+	function initializeCurrent(latcurr, longcurr) {
+		currgeocoder = new google.maps.Geocoder();
+		if (latcurr != '' && longcurr != '') {
+			var myLatlng = new google.maps.LatLng(latcurr, longcurr);
+			return getCurrentAddress(myLatlng);
+		}
+	}
+	//Get current address
+	function getCurrentAddress(location) {
+		currgeocoder.geocode({
+			'location': location
+		}, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				currentloc.push(results[0].formatted_address);
+				socket.emit("location", {
+					location: currentloc
+				});
+			}
+			else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+	}
 });
-(function ($) {
-    console.log($('#match').html());;
-    //console.log(match);
-})(jQuery);
+var table = $('table');
+$('#location, #popscore , #age, #firstname').wrapInner('<span title="sort this column"/>').each(function () {
+	var th = $(this)
+		, thIndex = th.index()
+		, inverse = false;
+	th.click(function () {
+		table.find('td').filter(function () {
+			return $(this).index() === thIndex;
+		}).sortElements(function (a, b) {
+			if (Number($.text([a])) && Number($.text([b]))) {
+				console.log("weeesh");
+				return $.text([a]) - $.text([b]) > 0 ? inverse ? -1 : 1 : inverse ? 1 : -1;
+			}
+			else {
+				return $.text([a]) > $.text([b]) ? inverse ? -1 : 1 : inverse ? 1 : -1;
+			}
+		}, function () {
+			return this.parentNode;
+		});
+		inverse = !inverse;
+	});
+});
--- server/create_account.js
+++ server/create_account.js
@@ -2,12 +2,24 @@ var moment = require('moment');
 var mysql = require('mysql');
 var md5 = require('md5');
 var uniqid = require('uniqid');
+
+function isValid(str) {
+	if (typeof (str) !== 'string') {
+		return false;
+	}
+	for (var i = 0; i < str.length; i++) {
+		if (str.charCodeAt(i) > 127) {
+			return false;
+		}
+	}
+	return true;
+}
 exports.form_checker = function (firstname, lastname, birthday, username, email, conf_email, password, conf_password, sexe) {
 	var ret = '';
 	if (!firstname) return ('Please enter a firstname');
 	else if (!lastname) return ('Please enter a lastname');
 	else if (!birthday) return ('Please enter a birthday');
-	else if (!username) return ('Please enter an username');
+	else if (!isValid(username)) return ('Please enter an username with only valids characters');
 	else if (!email) return ('Please enter an email');
 	else if (!conf_email) return ('Please enter an email confirmation');
 	else if (!password) return ('Please enter a password');
